36e4f30e976b6111d9055f8326feff5dfdd88754
younssue <love_rvd7@naver.com>
2024-07-16 13:05:07 +0900

    :bug:  Fix dto constructor error [#13]

--- src/main/java/org/dessert/moah/order/dto/OrderRequestDto.java
+++ src/main/java/org/dessert/moah/order/dto/OrderRequestDto.java
@@ -2,11 +2,19 @@ package org.dessert.moah.order.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
+@NoArgsConstructor
 public class OrderRequestDto {
     private Long dessertId;
     private int count;
     private String deliveryAddress;
+
+    @Builder
+    public OrderRequestDto(Long dessertId, int count, String deliveryAddress) {
+        this.dessertId = dessertId;
+        this.count = count;
+        this.deliveryAddress = deliveryAddress;
+    }
 }
